b0f7ff3c1c1ab9f8bd5df338bea1da5047ccdc28
pas28 <pas28@omnino-28.dcs.aber.ac.uk>
2015-09-25 11:21:24 +0100

    This is the eclipse project for our conversation. Main method has been implemented, as well as paulsPart()

---
+++ src/PaulAndMichael_Conversation/src/MainConversation.java
@@ -0,0 +1,13 @@
+
+public class MainConversation {
+	
+	public void paulsPart() {
+		System.out.println("Hello michael!");
+	}
+
+	public static void main(String[] args) {
+		MainConversation convo = new MainConversation();
+		convo.paulsPart();
+	}
+
+}
