481112cd70205f769fcd19f9189669eb45849453
mwr2 <mwr2@omnino-29.dcs.aber.ac.uk>
2015-09-25 11:42:47 +0100

    hehhe

--- src/PaulAndMichael_Conversation/src/MainConversation.java
+++ src/PaulAndMichael_Conversation/src/MainConversation.java
@@ -4,9 +4,11 @@ public class MainConversation {
 	public void paulsPart() {
 		System.out.println("Hello michael!");
 	}
-	 public void MichaelsResponce(){
-		 System.out.println("get lost");
-	 }
+	
+	public void MichaelsResponce(){
+		 System.out.println("get lost, your just a scrub");
+	}
+	
 	public static void main(String[] args) {
 		MainConversation convo = new MainConversation();
 		convo.paulsPart();
