9c62a57b19c8bf4a5bb909835bbd28e23ebbcc8c
mwr2 <mwr2@omnino-29.dcs.aber.ac.uk>
2015-09-25 11:35:55 +0100

    changed....

--- src/PaulAndMichael_Conversation/src/MainConversation.java
+++ src/PaulAndMichael_Conversation/src/MainConversation.java
@@ -4,10 +4,13 @@ public class MainConversation {
 	public void paulsPart() {
 		System.out.println("Hello michael!");
 	}
-
+	 public void MichaelsResponce(){
+		 System.out.println("get lost");
+	 }
 	public static void main(String[] args) {
 		MainConversation convo = new MainConversation();
 		convo.paulsPart();
+		convo.MichaelsResponce();
 	}
 
 }
