8a262b3d60a492444071e9899a3005397e1a47dc
thm17 <thm17@omnino-30.dcs.aber.ac.uk>
2015-09-25 11:53:31 +0100

    asd

--- src/PaulAndMichael_Conversation/src/MainConversation.java
+++ src/PaulAndMichael_Conversation/src/MainConversation.java
@@ -1,18 +1,24 @@
 
 public class MainConversation {
-	
+
 	public void paulsPart() {
 		System.out.println("Hello michael!");
 	}
-	
-	public void MichaelsResponce(){
-		 System.out.println("get lost, your just a scrub");
+
+	public void MichaelsResponce() {
+		System.out.println("get lost, your just a scrub");
 	}
-	
+
+	public void TomPart() {
+		System.out.println("Yay for scrubs!");
+		System.out.println("They are cool");
+	}
+
 	public static void main(String[] args) {
 		MainConversation convo = new MainConversation();
 		convo.paulsPart();
 		convo.MichaelsResponce();
+		convo.TomPart();
 	}
 
 }
